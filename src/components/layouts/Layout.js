a85279e31a23f57537ca7b8078177acd793489ed
Vladimir <vladimirg@grabit.mk>
2022-02-01 11:16:00 +0100

    Fix dark mode background color

--- src/components/layouts/Layout.js
+++ src/components/layouts/Layout.js
@@ -4,7 +4,7 @@ import "./Layout.css"
 
 function Layout(props) {
     return (
-        <div className="flex flex-col min-h-screen bg-stone-200 bg-opacity-50 dark:bg-zincx-900">
+        <div className="flex flex-col min-h-screen bg-stone-200 bg-opacity-50 dark:bg-zinc-900">
             <Navbar />
             <main>
                 {props.children}
